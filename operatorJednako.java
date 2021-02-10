abe5f489a31bbc53efa1243b2c74847b17b53516
Mladen <42699553+MicmladenTo@users.noreply.github.com>
2021-02-10 19:01:07 +0100

    Add files via upload

---
+++ operatorJednako.java
@@ -0,0 +1,39 @@
+package operatorJednako;
+
+public class operatorJednako {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		
+		a = b;
+		
+		b = 8;
+		
+		System.out.println("vrednost a je: " + a);
+		
+		// a = b = 12; - Sve troje su 12!
+		
+		/*	
+	 	int a = 5;
+		int b = 7;
+		int privremena;
+		
+		privremena = a;
+		a = b;
+		b = privremena;
+		*/
+		
+		/*
+		int a = 15;
+		int b = 7;
+		double kolicnik = (double) a / b;
+		// u suprotnom, jedan od integera mora da bude double
+		System.out.println("vrednost a je " + a + " vrednost b je " + b + " a kolicnik je " + kolicnik);
+		*/
+		
+
+	}
+
+}
